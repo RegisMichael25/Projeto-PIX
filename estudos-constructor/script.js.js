8922f0fb97f06b0eda3cb31b8568f866bd321e84
Regis Michael <regis.michael101@gmail.com>
2024-10-18 22:40:20 -0300

    Incorporando data e hora no momento do envio.

--- estudos-constructor/script.js.js
+++ estudos-constructor/script.js.js
@@ -1,8 +1,11 @@
     class ContaBancaria{
-        constructor (nome, valorNaConta, data, hora)
+        constructor (nome, valorNaConta)
         {
             this.nome = nome
             this.valorNaConta = valorNaConta
+            this.data = new Date();
+            this.data.ano = `${this.data.getFullYear()}/${this.data.getMonth() + 1}/${this.data.getDate()}`
+            this.data.horas = `${this.data.getHours()}:${this.data.getMinutes()}:${this.data.getSeconds()}`
         }
 
         transferenciaPix(valorEnviado, beneficiario)
@@ -19,15 +22,14 @@
                 console.log(`Você enviou uma transferência para ${beneficiario} de R$ ${valorEnviado.toFixed(2)}`);
                 this.valorNaConta -= valorEnviado  
                 console.log(`Seu saldo na conta atual é de ${this.valorNaConta.toFixed(2)}`)
-                console.log('Pix enviado com sucesso!')
-                console.log('Enviado: ', valorEnviado.toFixed(2))
+                console.log(`Pix enviado com sucesso! ${this.data.ano} - ${this.data.horas}`)
                 return parseFloat(valorEnviado.toFixed(2))
             }
         }
         
         recebimentoPix(valorRecebido,remetente)
         {
-            if(valorRecebido == 0 || valorRecebido < 0)
+            if(valorRecebido <= 0)
             {
                 console.log('Nenhum envio.')
             }
@@ -40,25 +42,10 @@
         }
     }
 
-    let dataAtual = new Date();
 
-    let ano = dataAtual.getFullYear();
-    let mes = dataAtual.getMonth() + 1;
-    let dia = dataAtual.getDate();
-
-    let horas = dataAtual.getHours();
-    let minutos = dataAtual.getMinutes();
-    let segundos = dataAtual.getSeconds();
-
-    let data = `${dia}/${mes}/${ano}`
-    let tempo = `${horas}:${minutos}:${segundos}`
-
-    console.log(data);
-    console.log(tempo);
-
-    // let regis = new ContaBancaria('Regis Michael', 400.00);
-    // let joao = new ContaBancaria('João Silas', 590.00);
+    let regis = new ContaBancaria('Regis Michael', 400.00);
+    let joao = new ContaBancaria('João Silas', 590.00);
 
     
-    // joao.recebimentoPix(regis.transferenciaPix(200.00, joao.nome), regis.nome);
-    // regis.recebimentoPix(joao.transferenciaPix(400.00, regis.nome), joao.nome);
+    joao.recebimentoPix(regis.transferenciaPix(200.00, joao.nome), regis.nome);
+    regis.recebimentoPix(joao.transferenciaPix(400.00, regis.nome), joao.nome);
