cd622328fe22bcfae668aa8098c7cfd83d60b2c0
Regis Michael <regis.michael101@gmail.com>
2024-10-19 09:00:36 -0300

    Tradução de variaveis pt-br / usa

--- estudos-constructor/script.js.js
+++ estudos-constructor/script.js.js
@@ -1,58 +1,58 @@
-    class ContaBancaria{
-        constructor (nome, valorNaConta)
+    class BankAccount{
+        constructor (username, amountInTheAccount)
         {
-            this.nome = nome
-            this.valorNaConta = valorNaConta
-            this.data = new Date();
-            this.data.ano = `${this.data.getFullYear()}/${this.data.getMonth() + 1}/${this.data.getDate()}`
-            this.data.horas = `${this.data.getHours()}:${this.data.getMinutes()}:${this.data.getSeconds()}`
+            this.username = username
+            this.amountInTheAccount = amountInTheAccount
+            this.transferDate = new Date();
+            this.transferDate.year = `${this.transferDate.getFullYear()}/${this.transferDate.getMonth() + 1}/${this.transferDate.getDate()}`
+            this.transferDate.hours = `${this.transferDate.getHours()}:${this.transferDate.getMinutes()}:${this.transferDate.getSeconds()}`
         }
 
-        transferenciaPix(valorEnviado, beneficiario)
+        pixTransfer(amountSent, beneficiary)
         {
-            if(beneficiario === this.nome)
+            if(beneficiary === this.username)
             {
                 console.log('Você não consegue enviar um valor para mesma titularidade.')
                 return 0
             }
             else
             {
-                if(valorEnviado > this.valorNaConta)
+                if(amountSent > this.amountInTheAccount)
                     {
                         console.log('Valor enviado maior que o que você possui em conta!')
-                        valorEnviado = 0
-                        return parseFloat(valorEnviado.toFixed(2))
+                        amountSent = 0
+                        return parseFloat(amountSent.toFixed(2))
                     }
                     else
                     {
-                        console.log(`Você enviou uma transferência para ${beneficiario} de R$ ${valorEnviado.toFixed(2)}`);
-                        this.valorNaConta -= valorEnviado  
-                        console.log(`Seu saldo na conta atual é de ${this.valorNaConta.toFixed(2)}`)
-                        console.log(`Pix enviado com sucesso! ${this.data.ano} - ${this.data.horas}`)
-                        return parseFloat(valorEnviado.toFixed(2))
+                        console.log(`Você enviou uma transferência para ${beneficiary} de R$ ${amountSent.toFixed(2)}`);
+                        this.amountInTheAccount -= amountSent  
+                        console.log(`Seu saldo na conta atual é de ${this.amountInTheAccount.toFixed(2)}`)
+                        console.log(`Pix enviado com sucesso! ${this.transferDate.ano} - ${this.transferDate.horas}`)
+                        return parseFloat(amountSent.toFixed(2))
                     }
             }
         }
         
-        recebimentoPix(valorRecebido,remetente)
+        pixReceipt(amountReceived,sender)
         {
-            if(valorRecebido <= 0)
+            if(amountReceived <= 0)
             {
                 console.log('Nenhum envio.')
             }
             else
             {
-                this.valorNaConta += valorRecebido
-                console.log('Total na conta do recebedor: ', this.valorNaConta.toFixed(2))
-                return console.log(`Você recebeu uma transferência de ${remetente} no valor de R$ ${valorRecebido.toFixed(2)}`);
+                this.amountInTheAccount += amountReceived
+                console.log('Total na conta do recebedor: ', this.amountInTheAccount.toFixed(2))
+                return console.log(`Você recebeu uma transferência de ${sender} no valor de R$ ${amountReceived.toFixed(2)}`);
             }
         }
     }
 
 
-    let regis = new ContaBancaria('Regis Michael', 400.00);
-    let joao = new ContaBancaria('João Silas', 590.00);
+    let regis = new BankAccount('Regis Michael', 400.00);
+    let joao = new BankAccount('João Silas', 590.00);
 
     
-    joao.recebimentoPix(joao.transferenciaPix(200.00, joao.nome), joao.nome);
-    // regis.recebimentoPix(joao.transferenciaPix(400.00, regis.nome), joao.nome);s
+    joao.pixReceipt(joao.pixTransfer(200.00, joao.username), joao.username);
+    // regis.pixReceipt(joao.pixTransfer(400.00, regis.username), joao.username);
